55dd0463ce153e4e5b89d6760be20aae9551aaf7
Aditya Addepalli <adi.addepalli@gmail.com>
2025-03-02 00:35:03 -0500

    feat: adding more metadata

--- www/src/app/layout.tsx
+++ www/src/app/layout.tsx
@@ -14,9 +14,43 @@ const spaceMono = Space_Mono({
 export const metadata: Metadata = {
 	title: siteConfig.name,
 	description: siteConfig.description,
-	metadataBase: new URL(
-		process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
-	),
+	metadataBase: new URL(siteConfig.url),
+	keywords: siteConfig.keywords,
+	authors: [{ name: siteConfig.creator.name, url: siteConfig.creator.url }],
+	creator: siteConfig.creator.name,
+	publisher: siteConfig.creator.name,
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+		},
+	},
+	icons: [{ rel: "icon", url: "/favicon.ico" }],
+	manifest: "/manifest.json",
+	appleWebApp: {
+		title: siteConfig.name,
+		statusBarStyle: "default",
+		capable: true,
+	},
+	openGraph: {
+		type: "website",
+		locale: siteConfig.locale,
+		url: siteConfig.url,
+		title: siteConfig.name,
+		description: siteConfig.description,
+		siteName: siteConfig.name,
+		images: [{ url: "/opengraph-image.png" }],
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: siteConfig.name,
+		description: siteConfig.description,
+		creator: siteConfig.creator.twitter,
+		site: siteConfig.creator.twitter,
+		images: [{ url: "/twitter-image.png" }],
+	},
 };
 
 export default function RootLayout({
