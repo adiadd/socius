466a2a5d4f71d61949ff0e8edfdb3c63b90fd4a5
Aditya Addepalli <adi.addepalli@gmail.com>
2025-03-01 21:36:02 -0500

    ui: adjusting color schemes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${spaceMono.variable}`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white font-mono text-sm">
+          <div className="min-h-screen bg-background text-black dark:text-white font-mono text-sm">
             <div className="max-w-4xl mx-auto p-4">
               <div className="border border-black dark:border-white">
                 {children}
